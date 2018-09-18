e379d9c286ca2f9a20d04b580a2fd1698e789e6c
Sebastien Dubois <seb@dsebastien.net>
2018-09-19 00:32:31 +0200

    The build script now copies the samples to the output dir. This is necessary for the html version

--- build.sh
+++ build.sh
@@ -2,11 +2,14 @@
 DESTINATION_DIR=./dist
 BOOK_FILE=README.adoc
 FODT_BOOK_FILE=README.fodt
+SAMPLES_DIR=samples
 
 rm -rf $DESTINATION_DIR
+mkdir -p $DESTINATION_DIR/$SAMPLES_DIR
+cp -r ./$SAMPLES_DIR $DESTINATION_DIR
 
 # Generate HTML
 asciidoctor --destination-dir $DESTINATION_DIR $BOOK_FILE
 
 # Generate fodt & format it using xmllint
-asciidoctor --destination-dir $DESTINATION_DIR --template-engine slim --template-dir slim --backend packt $BOOK_FILE && xmllint -format $DESTINATION_DIR/$FODT_BOOK_FILE
+asciidoctor --destination-dir $DESTINATION_DIR --template-engine slim --template-dir slim --backend packt $BOOK_FILE && xmllint -format $DESTINATION_DIR/$FODT_BOOK_FILE 2>&1 > /dev/null
